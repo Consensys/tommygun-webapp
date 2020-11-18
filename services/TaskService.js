03e35ba8335be436ba84a7c745182e59e09fc279
Abdelhamid Bakhta <abdelhamid.bakhta@consensys.net>
2020-11-18 17:36:59 +0100

    Update TaskService.js

--- services/TaskService.js
+++ services/TaskService.js
@@ -2,16 +2,22 @@ const axios = require('axios').default;
 
 export default class TaskService {
     constructor(settings) {
-        this.apiRoot = `${settings.backend.url}/${settings.backend.taskAPIRoot}`;
-        this.getAllTasksEndpoint = this.apiRoot;
+        this.settings = settings;
+        this.refreshConfig();
     }
 
     async getAll() {
         try {
+            this.refreshConfig();
             const response = await axios.get(this.getAllTasksEndpoint);
             return response.data;
         } catch (error) {
             console.log("error", error);
         }
+    }
+
+    refreshConfig(){
+        this.apiRoot = `${this.settings.backend.url}/${this.settings.backend.taskAPIRoot}`;
+        this.getAllTasksEndpoint = this.apiRoot;
     }
 }
