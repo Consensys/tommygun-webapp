db77194cb7d2469a9ea45374daf38bc908bde624
Abdelhamid Bakhta <abdelhamid.bakhta@consensys.net>
2020-11-23 18:03:23 +0100

    Update Task.vue

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,12 +1,23 @@
 <template>
   <div class="ml-4 mr-4 mt-4">
-    <b-table
-        :fields="fields"
-        :items="tasks"
-        :tbody-tr-class="rowClass"
-        head-variant="dark"
-        striped>
-    </b-table>
+    <div  v-if="displayTasks()">
+      <b-table
+          :fields="fields"
+          :items="tasks"
+          :tbody-tr-class="rowClass"
+          head-variant="dark"
+          striped>
+      </b-table>
+    </div>
+    <div v-else>
+      <b-table
+          disabled
+          :fields="fields"
+          head-variant="dark"
+          striped>
+      </b-table>
+    </div>
+
   </div>
 </template>
 
@@ -24,23 +35,39 @@ export default {
         {key: 'end', sortable: true},
         {key: 'duration', sortable: true}],
       tasks: [],
+      lastTasks: [],
+      timer: null,
     }
   },
-  async mounted() {
-    const tasks = await this.$store.state.services.task.getAll();
-    tasks.forEach(task => {
-      this.tasks.push({
-        identifier: task.id,
-        name: task.name,
-        status: task.status,
-        start: task.startedAt,
-        end: task.endedAt,
-        errorMessage: task.errorMessage,
-        duration: task.durationMillis,
-      });
-    });
+  created() {
+    this.refreshTasks();
+    this.timer = setInterval(this.refreshTasks, 500)
+  },
+  beforeDestroy() {
+    clearInterval(this.timer)
   },
   methods: {
+    async displayTasks(){
+      return Array.isArray(this.tasks) && this.tasks.length;
+    },
+    async refreshTasks(){
+      const tasks = await this.$store.state.services.task.getAll();
+      if(JSON.stringify(tasks) !== JSON.stringify(this.lastTasks)) {
+        this.tasks = [];
+        tasks.forEach(task => {
+          this.tasks.push({
+            identifier: task.id,
+            name: task.name,
+            status: task.status,
+            start: task.startedAt,
+            end: task.endedAt,
+            errorMessage: task.errorMessage,
+            duration: task.durationMillis,
+          });
+        });
+        this.lastTasks = tasks;
+      }
+    },
     rowClass(item, type) {
       if (!item || type !== 'row') {
         return;
